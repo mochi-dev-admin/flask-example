063a2627dafaa654f6194abe1ffbdfdf7877c25d
Pratyusha Gogulapati <pratyusha@Pratyushas-MacBook-Pro.local>
2022-05-02 14:20:53 -0700

    small change to string 2

--- application.py
+++ application.py
@@ -4,5 +4,3 @@ application = Flask(__name__)
 @application.route('/')
 def hello_world():
 	return 'Hi Myra, wassup this is our web app.'
-
-personal_access_token = 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
